3a7a0644e67ad1a4ec7d104c9e0389ac26206cb9
MaryamJami1 <maryamchouhan373@gmail.com>
2024-08-11 21:08:59 +0500

    bulb

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 
-let imgON = document.getElementById("img")
-function img(){
-   imgON.src="bulb.png"
-   imgON.className="styling"
-   document.getElementById("on").disabled= true
-   document.getElementById("off").disabled= false
+let imgON = document.getElementById("img");
+
+function img() {
+   imgON.src = "bulb.png";
+   imgON.className = "styling";
+   document.getElementById("on").disabled = true;
+   document.getElementById("off").disabled = false;
+   document.getElementById("body").className=" backgroundOn"
 }
 
-
-function imgOff(){
-imgON.src="Bulb light png.jfif"
-document.getElementById("on").disabled= false
-document.getElementById("off").disabled= true
+function imgOff() {
+   imgON.src = "Bulb_light_png-removebg-preview.png";
+   document.getElementById("on").disabled = false;
+   document.getElementById("off").disabled = true;
+  document.getElementById("body").className=" backgroundOf"
 }
